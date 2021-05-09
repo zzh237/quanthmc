eccd0042be358869d59c1515bf59aecf437db2ca
zhi zhang <zhizhang@umd.edu>
2021-05-09 01:12:38 -0400

    added

--- run2.sh
+++ run2.sh
@@ -11,6 +11,6 @@ for n in 0.1
 do
 for m in 0 
 do
-	python experiments/ind_exp.py --algo_name HMC --data_name iris --model_name Quant --mlp_depth $m  --tr_ratio $n --step_size 0.1 --L 20 --num_samples 100 
+	python3 experiments/ind_exp.py --algo_name HMC --data_name iris --model_name Quant --mlp_depth $m  --tr_ratio $n --step_size 0.1 --L 20 --num_samples 100 
 done
 done
--- run4.sh
+++ run4.sh
@@ -7,10 +7,10 @@ pip3 install matplotlib
 pip3 install -U scikit-learn
 pip3 install termcolor
 export PYTHONPATH=$PYTHONPATH:/zzproject/quanthmc
-for n in 0.01 0.03 0.05 0.07
+for n in 0.01 
 do
 for m in 0
 do
-	python experiments/ind_exp.py --algo_name HMC --data_name mnist_two_target --model_name Quant --mlp_depth $m  --tr_ratio $n --step_size 0.1 --L 20 --num_samples 100 
+	python3 experiments/ind_exp.py --algo_name HMC --data_name mnist_two_target --model_name Quant --mlp_depth $m  --tr_ratio $n --step_size 0.1 --L 20 --num_samples 100 
 done
 done
