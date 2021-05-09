addfaaa5dd750970afe781aa9ed5b5fecaa7a63b
zhi zhang <zhizhang@umd.edu>
2021-05-09 09:52:26 -0400

    new

--- run2.sh
+++ run2.sh
@@ -1,11 +1,4 @@
 cd /zzproject/quanthmc
-apt-get -y install cmake protobuf-compiler
-python3 -m pip install --user --upgrade pip
-# pip3 install qulacs-gpu
-pip3 install pennylane-qulacs["gpu"]
-pip3 install matplotlib
-pip3 install -U scikit-learn
-pip3 install termcolor
 export PYTHONPATH=$PYTHONPATH:/zzproject/quanthmc
 for n in 0.1
 do
